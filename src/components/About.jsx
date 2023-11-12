2e70eb175b16faa39dee8f03fa75128ec28abc83
[j chun] <[justinchun224@gmail.com]>
2023-11-11 23:51:47 -0800

    added animation to about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,7 +14,7 @@ export default function About () {
 
         const options = {
             threshold: 1,
-            rootMargin: '0px 0px -200px 0px',
+            rootMargin: '0px 0px -150px 0px',
         };
 
         const observer = new IntersectionObserver(callback, options);
@@ -23,7 +23,28 @@ export default function About () {
             observer.observe(card)
         });
 
-        return () => {
+        // Slide Animation
+
+        const startAnimation = (event) => {
+            const target = event.currentTarget;
+            const text = target.querySelector('.slide-about');
+            text.style.animation = 'slideRight 2s linear infinite';
+        }
+
+        const stopAnimation = (event) => {
+            const target = event.currentTarget;
+            const text = target.querySelector('.slide-about');
+            text.style.animation = 'none';
+        }
+
+        const containers = document.querySelectorAll('.slide-container');
+
+        containers.forEach(container => {
+            container.addEventListener('mouseover', startAnimation);
+            container.addEventListener('mouseleave', stopAnimation);
+        });
+
+        return () => {s
             observer.disconnect();
         };
 
@@ -33,27 +54,45 @@ export default function About () {
         <main className="about-container">
             <section>
                 <div className="about-card">a little bit about myself</div>
-                <div className="about-info">I AM</div>
+                <div className="slide-container">
+                    <div className="slide-about">I AM</div>
+                </div>
             </section>
             <section>
                 <div className="about-card">text</div>
-                <div>BASED IN LOS ANGELES</div>
+                <div className="slide-container">
+                    <div className="slide-about">BASED IN LOS ANGELES</div>
+                </div>
             </section>
             <section>
                 <div className="about-card">text</div>
-                <div>FRONT END ADDICTED</div>
+                <div className="slide-container">
+                    <div className="slide-about">FRONT END ADDICTED</div>
+                </div>
             </section>
             <section>
                 <div className="about-card">text</div>
-                <div>FULL STACK CAPABLE</div>
+                <div className="slide-container">
+                    <div className="slide-about">FULL STACK CAPABLE</div>
+                </div>
             </section>
             <section>
                 <div className="about-card">text</div>
-                <div>LOVER OF DRY CODE</div>
+                <div className="slide-container">
+                    <div className="slide-about">LOVER OF DRY CODE</div>
+                </div>
             </section>
             <section>
                 <div className="about-card">text</div>
-                <div>ASPIRATIONAL GRAPHIC DESIGNER</div>
+                <div className="slide-container">
+                    <div className="slide-about">ASPIRATIONAL GRAPHIC DESIGNER</div>
+                </div>
+            </section>
+            <section>
+                <div className="about-card">text</div>
+                <div className="slide-container">
+                    <div className="slide-about">PART-TIME HOOPER</div>
+                </div>
             </section>
         </main>
     )
