533ceac725b6bf64786cecc76932ff154d65ab6f
[j chun] <[justinchun224@gmail.com]>
2023-11-12 11:14:01 -0800

    added useState and clear form function

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,15 +1,29 @@
+import { useState } from 'react';
+
 export default function Contact() {
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+
+    // For now this only clears input fields
+    const handleFormSubmit = () => {
+        setName('');
+        setEmail('');
+        setMessage('');
+    }
+
     return (
         <main>
             <h2 className="contact-header">Contact</h2>
             <section className="contact-form">
                 <label>Name</label>
-                <input type="text"></input>
+                <input type="text" value={name} onChange={(e) => setName(e.target.value)}></input>
                 <label>Email</label>
-                <input type="text" ></input>
+                <input type="text" value={email} onChange={(e) => setEmail(e.target.value)}></input>
                 <label>Message</label>
-                <textarea id="contactMsg" rows="3"></textarea>
-                <button>Let's Connect!</button>
+                <textarea id="contactMsg" rows="3" value={message} onChange={(e) => setMessage(e.target.value)}></textarea>
+                <button id="contactBtn" onClick={handleFormSubmit}>Let's Connect!</button>
             </section>
         </main>
     )
